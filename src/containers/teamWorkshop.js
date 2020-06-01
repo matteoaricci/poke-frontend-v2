d824efc17aad0d0690556c10e7533551952fec36
matteoaricci <matteo.a.ricci@gmail.com>
2020-05-31 20:33:52 -0400

    Add pokemon btn working

--- src/containers/teamWorkshop.js
+++ src/containers/teamWorkshop.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import {Button} from 'react-bootstrap'
 class teamWorkshop extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            team: []
+        }
+    }
+
+    componentDidMount() {
+        const teamId = this.props.location.state.currentTeam
+
+        fetch(`http://localhost:3001/pokeonteam/${teamId}`)
+        .then(resp => resp.json())
+        .then(team => this.setState({team}))
+    }
+
+    addPokemonClick = () => {
+        const teamId = this.props.location.state.currentTeam
+
+        fetch('http://localhost:3001/poke_on_teams', {
+            method: 'POST',
+            headers: {
+                "Content-type": 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({user_team_id: teamId})
+        })
+        .then(resp => resp.json())
+        .then(poke => this.setState({team: [...this.state.team, poke]}))
+    }
+
     render() {
         return (
             <div>
-                {/* Show Current Pokemon in Team  */}
-                {/* Need to Have Btn right next to it to add Pokemon */}
-                <Button>Add Pokemon</Button>
+                <div className='pokemon-on-team'>
+                    {this.state.team.map(member => <div>{member.user_team_id}</div>)}
+                    <Button onClick={this.addPokemonClick} >Add Pokemon</Button>
+                </div>
 
             </div>
         );
